f05e0329f2f08735157d592484f42eba60a9ca8f
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-15 11:20:42 +0100

    Initial Commit

---
+++ Storage User Defaults/Storage_User_DefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Storage_User_DefaultsApp.swift
+//  Storage User Defaults
+//
+//  Created by CHEBIHI FAYCAL on 15/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Storage_User_DefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
